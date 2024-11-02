9cab32c82a5559c8c25a04d962fa27c9b60f2b24
Besjana Jaçaj <b.jacaj1@gmail.com>
2024-11-02 15:27:04 +0100

    add test for organisation/location endpoint

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -8,16 +8,19 @@ import alembic.config
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
+from sqlmodel import select
 
 from app.db import get_database_session
 from app.main import app
-from app.models import Organisation
+from app.models import Organisation, Location
 
 _ALEMBIC_INI_PATH = Path(__file__).parent.parent / "alembic.ini"
+
 
 @pytest.fixture()
 def test_client() -> TestClient:
     return TestClient(app)
+
 
 @pytest.fixture(autouse=True)
 def apply_alembic_migrations() -> Generator[None, None, None]:
@@ -61,4 +64,44 @@ def test_organisation_endpoints(test_client: TestClient) -> None:
     # Validate that created organisations can be retried via API
     response = test_client.get("/api/organisations")
     organisations = set(organisation["name"] for organisation in response.json())
-    assert  set(organisations) == created_organisation_names
+    assert set(organisations) == created_organisation_names
+
+
+def test_organisation_locations_endpoints(test_client: TestClient) -> None:
+    list_of_locations = [{"location_name": "location1",
+                          "organisation_id": 1,
+                          "longitude": 0,
+                          "latitude": 0}, {"location_name": "location2",
+                                           "organisation_id": 2,
+                                           "longitude": 0,
+                                           "latitude": 0}, {"location_name": "location3",
+                                                            "organisation_id": 1,
+                                                            "longitude": 0,
+                                                            "latitude": 0}, ]
+
+    with get_database_session() as database_session:
+        locations_before = database_session.query(Location).all()
+        database_session.expunge_all()
+    assert len(locations_before) == 0
+
+    list_of_organisation_names_to_create = ["organisation_a", "organisation_b"]
+
+    for organisation_name in list_of_organisation_names_to_create:
+        response = test_client.post("/api/organisations/create", json={"name": organisation_name})
+        assert response.status_code == status.HTTP_200_OK
+
+    for location in list_of_locations:
+        response = test_client.post("/api/organisations/create/locations", json=location)
+        assert response.status_code == status.HTTP_200_OK
+
+    # Validate that organisations exist in database
+    with get_database_session() as database_session:
+        organisation_locations_after = database_session.exec(
+            select(Location).where(Location.organisation_id == 1)).all()
+        database_session.expunge_all()
+
+    response = test_client.get(f"/api/organisations/1/locations")
+    locations_name = set(x["location_name"] for x in response.json())
+
+    for el in zip(organisation_locations_after, locations_name):
+        assert el[0].location_name == el[1]
